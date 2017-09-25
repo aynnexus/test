6c36a3ded2c94e5a5553d8eb727c1ccb8882f587
fiberno <fiberno@gmail.com>
2017-09-25 15:56:14 +0000

    server

--- config/global.php
+++ config/global.php
@@ -7,7 +7,7 @@ define('REQUIRE', 2);
 define('NOTREQUIRE', 1);
 define('FIRST_AUTH', 2);
 define('MEMBER_AUTH', 60);
-define('unifiServer', 'https://128.199.68.187:8443');
+define('unifiServer', 'https://174.138.16.149:8443');
 define('unifiUser', 'Nexus');
 define('unifiPass', 'Nexus123!@#');
 define('unifiSite', 'detault');
