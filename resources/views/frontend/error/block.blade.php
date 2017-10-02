08f916d639640746d88cdd411f5dd3802a26b59b
fiberno <fiberno@gmail.com>
2017-10-02 17:21:13 +0000

    update server

---
+++ resources/views/frontend/error/block.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+@section('content')
+<!-- Main content --><br><br><br><br><br><br><br><br>
+    <section class="content">
+
+      <div class="error-page">
+        <h2 class="headline text-red">500</h2>
+
+        <div class="error-content">
+          <h3><i class="fa fa-warning text-red"></i> Oops! Something went wrong.</h3>
+
+          <p>
+            We will work on fixing that right away.
+          </p>
+          
+        </div>
+      </div>
+      <!-- /.error-page -->
+
+    </section>
+    <!-- /.content -->
+@endsection
