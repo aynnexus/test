6e039ad4ad945f5da0801be9b88bc6cc9743fc4f
fiberno <fiberno@gmail.com>
2017-11-14 17:02:55 +0000

    update pull

--- config/services.php
+++ config/services.php
@@ -37,8 +37,8 @@ return [
     ],
 
     'facebook'=> [        
-        'client_id'     => '1974724386150653',
-        'client_secret' => '86cf30b512c4bd2f497e9b3815fec90a',
+        'client_id'     => '330008387465431',
+        'client_secret' => 'b72439a9a10ee2131b75045c6ce1dab6',
         'redirect'      => 'http://174.138.16.149/guest/login/facebook/callback'
     ],
 
